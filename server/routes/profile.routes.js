18cbdac33f83b1d01b24dded7d9690aa727024de
Fengyuan Yang <fyyang285@gmail.com>
2025-07-30 19:34:32 -0400

    first commit

--- server/routes/profile.routes.js
+++ server/routes/profile.routes.js
@@ -7,6 +7,6 @@ router.post('/api/profile', profileController.createProfile); // POST /api/profi
 router.get('/api/profile/users/:userId', profileController.getProfileById); // GET /api/profile/users/:userId
 router.get('/api/profile', profileController.getProfiles); // GET /api/profile
 router.put('/api/profile/users/:userId', profileController.updateProfile); // PUT /api/profile/users/:userId
-router.delete('/api/profile/users/:userId', profileController.deleteProfile); // DELETE /api
+router.delete('/api/profile/users/:userId', profileController.deleteProfile); // DELETE /api/profile/users/:userId
 
 export default router; 
